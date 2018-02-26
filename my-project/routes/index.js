7574aef1e615d2a89395684401d18cf37e79d723
yangdexuan <zhichao520836>
2018-02-26 23:16:21 +0800

    ECSHOP管理中心页面顶部静态页面

--- my-project/routes/index.js
+++ my-project/routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('login', { title: '登录页面' });
 });
+router.get('/main', function(req, res, next) {
+  res.render('main', { title: 'ECSHOP管理中心' });
+});
 
 module.exports = router;
